7f236ef23e27f039b9c924c9b07c165d9d3ab7eb
senpailys <alyssahammonds5453@gmail.com>
2023-12-14 17:25:07 -0600

    basic functionaility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pybricks-micropython
-import random
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -8,4 +7,41 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
+# this program will move the that is holding a pen to draw a polygon
+
 ev3 = EV3Brick()
+#beep
+ev3.speaker.beep()
+
+# motors
+shoulder = Motor(Port.A)
+elbow = Motor(Port.B)
+claw = Motor(Port.C)
+
+# sensors
+touch = TouchSensor(Port.S1)
+
+# constants
+shoulder_speed = 100
+elbow_speed = 100
+claw_speed = 100
+
+# functions
+def move_shoulder(degrees):
+    shoulder.run_target(shoulder_speed, degrees)
+
+def move_elbow(degrees):
+    elbow.run_target(elbow_speed, degrees)
+
+
+# center shoulder
+ev3.speaker.say("centering shoulder")
+while not touch.pressed():
+    shoulder.run(shoulder_speed)
+shoulder.stop(Stop.HOLD)
+
+# move elbow
+# this goes down
+elbow.run_target(elbow_speed, 90)
+
+
